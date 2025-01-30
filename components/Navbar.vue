1555b08e39cf2d2d39880138820b6f4e10768ae1
Allan Niles <allancoding.dev@gmail.com>
2025-01-30 17:31:20 +0000

    navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar blur">
+    <a
+      href="javascript:void(0);"
+      class="icon"
+      id="navbarIcon"
+      onclick="toggleNavbar()"
+    >
+      &#9776;
+    </a>
+    <div class="links" id="navbarLinks">
+      <a href="#home">Home</a>
+      <a href="websites">Websites</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+    </div>
+    <a href="#home" class="logo">
+      <img src="/logos/allancoding.png" id="main-logo" alt="Logo" />
+      Allan Coding!
+    </a>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,7 @@
     <title>Allan Coding!</title>
     <div>
         <canvas id="cnv"></canvas>
-        <div class="navbar blur">
-            <a href="javascript:void(0);" class="icon" id="navbarIcon" onclick="toggleNavbar()">
-                &#9776;
-            </a>
-            <div class="links" id="navbarLinks">
-                <a href="#home">Home</a>
-                <a href="websites">Websites</a>
-                <a href="#about">About</a>
-                <a href="#contact">Contact</a>
-            </div>
-            <a href="#home" class="logo">
-                <img src="/logos/allancoding.png" id="main-logo" alt="Logo">
-                Allan Coding!
-            </a>
-        </div>
+        <Navbar/>
         <div class="content">
             <div class="home">
                 <h1>allancoding</h1>
